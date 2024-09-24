91fbb8f6e157be77dbb4507d07a24558fcb32510
josh1710 <joshuaribeiro17@gmail.com>
2024-09-24 14:55:57 +0200

    24/09

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,37 +15,37 @@ const Main = () => {
     <section class="weather-container">
         <div class="city" id="villejuif">
             <h2>Villejuif</h2>
-            <img src="https://em-content.zobj.net/source/apple/118/black-sun-with-rays_2600.png" alt="Soleil"/>
-            <p>Température : 20°C</p>
-            <p>Conditions : Ensoleillé</p>
+            {/* <img src="https://em-content.zobj.net/source/apple/118/black-sun-with-rays_2600.png" alt="Soleil"/> */}
+            <p>🌡️ : 20°C</p>
+            <p>☀️  Ensoleillé</p>
         </div>
 
         <div class="city" id="paris">
             <h2>Paris</h2>
             <i class="fas fa-cloud"></i>
-            <p>Température : 22°C</p>
-            <p>Conditions : Nuageux</p>
+            <p>🌡️ : 22°C</p>
+            <p>☁️  Nuageux</p>
         </div>
 
         <div class="city" id="lyon">
             <h2>Lyon</h2>
             <i class="fas fa-cloud-rain"></i>
-            <p>Température : 24°C</p>
-            <p>Conditions : Pluie légère</p>
+            <p>🌡️ : 24°C</p>
+            <p>🌧️  Pluie légère</p>
         </div>
 
         <div class="city" id="marseille">
             <h2>Marseille</h2>
             <i class="fas fa-sun"></i>
-            <p>Température : 28°C</p>
-            <p>Conditions : Ensoleillé</p>
+            <p>🌡️ : 28°C</p>
+            <p>☀️  Ensoleillé</p>
         </div>
 
         <div class="city" id="montpellier">
             <h2>Montpellier</h2>
             <i class="fas fa-cloud-sun"></i>
-            <p>Température : 27°C</p>
-            <p>Conditions : Partiellement nuageux</p>
+            <p>🌡️ : 27°C</p>
+            <p>☁️  Nuageux</p>
         </div>
     </section>
     </main>
